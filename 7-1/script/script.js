c56926e3e8c75da582078455dfbce49f92ac7a07
mao-onishi <mao011800@gmail.com>
2021-01-21 03:46:36 +0900

    chapter7 CSS追加

--- 7-1/script/script.js
+++ 7-1/script/script.js
@@ -2,28 +2,28 @@ new Vue({
     el: '#app',
     data() {
         return {
-            todos: [],
-            text: ''
+            todos: [],  //TODOリストを1件ずつ配列要素として格納
+            text: ''　　//入力フォームのテキストボックスと連動させる
         };
     },
     methods: {
-        inputText(e) {
-            this.text = e.target.value;
+        inputText(e) {  //テキストボックスからの入力を引数として受け取る
+            this.text = e.target.value;  //data()内に定義したtextプロパティにセット
         },
         addTodo() {
             if (!this.text) return;  //textプロパティが空の場合、以降の処理をしない
 
-            const text = this.text;  //textプロパティの値を変数textに代入
+            const text = this.text;  //textプロパティの値を定数textに格納
             const id = Math.ceil(Math.random() * 1000);  //乱数を発生。idとして使用
             const todo = {
                 id,  //TODOを一意に識別する。
                 text,  //テキストボックスの内容
                 isDone: false  //完了状態の保持
             };
-            this.todos.push(todo);  //配列todosに新規追加したTODOオブジェクトを追加
+            this.todos.push(todo);  //配列todosに新規追加したTODOオブジェクトを格納
             this.resetText();  //テキストボックスの内容を空にする。
         },
-        resetText() {
+        resetText() {  //テキストボックスの文字を自動的にクリアするメソッド
             this.text ='';
         },
         deleteTodo(id) {
@@ -40,7 +40,7 @@ new Vue({
             return index;
         }
     },
-    computed: {
+    computed: {  //差分を察知し、自動で処理する
         doneTodo() {
             return this.todos.filter( todo => todo.isDone === true);
         },
